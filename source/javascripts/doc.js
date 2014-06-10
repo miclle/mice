120b1dc6ff997a361b404824a09587ef8f9d12ba
miclle <miclle.zheng@gmail.com>
2014-06-11 00:00:19 +0800

    Update doc.js

--- source/javascripts/doc.js
+++ source/javascripts/doc.js
@@ -1,10 +1,18 @@
-//= require mice
 //= require mice/jquery
+//= require mice
 //= require_tree .
 
 // ZeroClipboard
 ZeroClipboard.config( { moviePath: '/images/ZeroClipboard.swf' } );
 
 $(function(){
+
+  if($("body.mobile").size()){
+    $(document).scroll(function() {
+      console.log('scroll')
+      //$('#window').contents().find('body').html($('#typography').next().next().text())
+    });
+  }
+  //
 
 });
