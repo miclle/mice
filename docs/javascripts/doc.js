9ba82cbf53e60a95839e8b69c5d59e2c6fc11bce
Miclle <miclle.zheng@gmail.com>
2014-08-15 00:11:38 +0800

    Update version to 0.2.10

--- docs/javascripts/doc.js
+++ docs/javascripts/doc.js
@@ -9744,287 +9744,248 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
   $(document).on('click.bs.alert.data-api', dismiss, Alert.prototype.close)
 
 }(jQuery);
-/* ========================================================================
- * Bootstrap: modal.js v3.2.0
- * http://getbootstrap.com/javascript/#modals
- * ========================================================================
- * Copyright 2011-2014 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
-
-
-+function ($) {
+(function() {
   'use strict';
-
-  // MODAL CLASS DEFINITION
-  // ======================
-
-  var Modal = function (element, options) {
-    this.options        = options
-    this.$body          = $(document.body)
-    this.$element       = $(element)
-    this.$backdrop      =
-    this.isShown        = null
-    this.scrollbarWidth = 0
-
-    if (this.options.remote) {
-      this.$element
-        .find('.modal-content')
-        .load(this.options.remote, $.proxy(function () {
-          this.$element.trigger('loaded.bs.modal')
-        }, this))
-    }
-  }
-
-  Modal.VERSION  = '3.2.0'
-
-  Modal.DEFAULTS = {
-    backdrop: true,
-    keyboard: true,
-    show: true
-  }
-
-  Modal.prototype.toggle = function (_relatedTarget) {
-    return this.isShown ? this.hide() : this.show(_relatedTarget)
-  }
-
-  Modal.prototype.show = function (_relatedTarget) {
-    var that = this
-    var e    = $.Event('show.bs.modal', { relatedTarget: _relatedTarget })
-
-    this.$element.trigger(e)
-
-    if (this.isShown || e.isDefaultPrevented()) return
-
-    this.isShown = true
-
-    this.checkScrollbar()
-    this.$body.addClass('modal-open')
-
-    this.setScrollbar()
-    this.escape()
-
-    this.$element.on('click.dismiss.bs.modal', '[data-dismiss="modal"]', $.proxy(this.hide, this))
-
-    this.backdrop(function () {
-      var transition = $.support.transition && that.$element.hasClass('fade')
-
-      if (!that.$element.parent().length) {
-        that.$element.appendTo(that.$body) // don't move modals dom position
-      }
-
-      that.$element
-        .show()
-        .scrollTop(0)
-
-      if (transition) {
-        that.$element[0].offsetWidth // force reflow
-      }
-
-      that.$element
-        .addClass('in')
-        .attr('aria-hidden', false)
-
-      that.enforceFocus()
-
-      var e = $.Event('shown.bs.modal', { relatedTarget: _relatedTarget })
-
-      transition ?
-        that.$element.find('.modal-dialog') // wait for modal to slide in
-          .one('miceTransitionEnd', function () {
-            that.$element.trigger('focus').trigger(e)
-          })
-          .emulateTransitionEnd(300) :
-        that.$element.trigger('focus').trigger(e)
-    })
-  }
-
-  Modal.prototype.hide = function (e) {
-    if (e) e.preventDefault()
-
-    e = $.Event('hide.bs.modal')
-
-    this.$element.trigger(e)
-
-    if (!this.isShown || e.isDefaultPrevented()) return
-
-    this.isShown = false
-
-    this.$body.removeClass('modal-open')
-
-    this.resetScrollbar()
-    this.escape()
-
-    $(document).off('focusin.bs.modal')
-
-    this.$element
-      .removeClass('in')
-      .attr('aria-hidden', true)
-      .off('click.dismiss.bs.modal')
-
-    $.support.transition && this.$element.hasClass('fade') ?
-      this.$element
-        .one('miceTransitionEnd', $.proxy(this.hideModal, this))
-        .emulateTransitionEnd(300) :
-      this.hideModal()
-  }
-
-  Modal.prototype.enforceFocus = function () {
-    $(document)
-      .off('focusin.bs.modal') // guard against infinite focus loop
-      .on('focusin.bs.modal', $.proxy(function (e) {
-        if (this.$element[0] !== e.target && !this.$element.has(e.target).length) {
-          this.$element.trigger('focus')
+  (function($) {
+    var Modal, Plugin, old;
+    Modal = (function() {
+      function Modal(element, options) {
+        this.options = options;
+        this.$body = $(document.body);
+        this.$element = $(element);
+        this.$backdrop;
+        this.isShown = null;
+        this.scrollbarWidth = 0;
+        if (this.options.remote) {
+          this.$element.find('.modal-content').load(this.options.remote, $.proxy((function() {
+            return this.$element.trigger('loaded.modal');
+          }), this));
         }
-      }, this))
-  }
-
-  Modal.prototype.escape = function () {
-    if (this.isShown && this.options.keyboard) {
-      this.$element.on('keyup.dismiss.bs.modal', $.proxy(function (e) {
-        e.which == 27 && this.hide()
-      }, this))
-    } else if (!this.isShown) {
-      this.$element.off('keyup.dismiss.bs.modal')
-    }
-  }
-
-  Modal.prototype.hideModal = function () {
-    var that = this
-    this.$element.hide()
-    this.backdrop(function () {
-      that.$element.trigger('hidden.bs.modal')
-    })
-  }
-
-  Modal.prototype.removeBackdrop = function () {
-    this.$backdrop && this.$backdrop.remove()
-    this.$backdrop = null
-  }
-
-  Modal.prototype.backdrop = function (callback) {
-    var that = this
-    var animate = this.$element.hasClass('fade') ? 'fade' : ''
-
-    if (this.isShown && this.options.backdrop) {
-      var doAnimate = $.support.transition && animate
-
-      this.$backdrop = $('<div class="modal-backdrop ' + animate + '" />')
-        .appendTo(this.$body)
-
-      this.$element.on('click.dismiss.bs.modal', $.proxy(function (e) {
-        if (e.target !== e.currentTarget) return
-        this.options.backdrop == 'static'
-          ? this.$element[0].focus.call(this.$element[0])
-          : this.hide.call(this)
-      }, this))
-
-      if (doAnimate) this.$backdrop[0].offsetWidth // force reflow
-
-      this.$backdrop.addClass('in')
-
-      if (!callback) return
-
-      doAnimate ?
-        this.$backdrop
-          .one('miceTransitionEnd', callback)
-          .emulateTransitionEnd(150) :
-        callback()
-
-    } else if (!this.isShown && this.$backdrop) {
-      this.$backdrop.removeClass('in')
-
-      var callbackRemove = function () {
-        that.removeBackdrop()
-        callback && callback()
       }
-      $.support.transition && this.$element.hasClass('fade') ?
-        this.$backdrop
-          .one('miceTransitionEnd', callbackRemove)
-          .emulateTransitionEnd(150) :
-        callbackRemove()
 
-    } else if (callback) {
-      callback()
-    }
-  }
+      return Modal;
 
-  Modal.prototype.checkScrollbar = function () {
-    if (document.body.clientWidth >= window.innerWidth) return
-    this.scrollbarWidth = this.scrollbarWidth || this.measureScrollbar()
-  }
+    })();
+    Modal.VERSION = '3.2.0';
+    Modal.DEFAULTS = {
+      backdrop: true,
+      keyboard: true,
+      show: true
+    };
+    Modal.prototype.toggle = function(_relatedTarget) {
+      if (this.isShown) {
+        return this.hide();
+      } else {
+        return this.show(_relatedTarget);
+      }
+    };
+    Modal.prototype.show = function(_relatedTarget) {
+      var e;
+      e = $.Event('show.modal', {
+        relatedTarget: _relatedTarget
+      });
+      this.$element.trigger(e);
+      if (this.isShown || e.isDefaultPrevented()) {
+        return;
+      }
+      this.isShown = true;
+      this.checkScrollbar();
+      this.$body.addClass('modal-open');
+      this.setScrollbar();
+      this.escape();
+      this.$element.on('click.dismiss.modal', '[data-dismiss="modal"]', $.proxy(this.hide, this));
+      return this.backdrop((function(_this) {
+        return function() {
+          var transition;
+          transition = $.support.transition && _this.$element.hasClass('fade');
+          if (!_this.$element.parent().length) {
+            _this.$element.appendTo(_this.$body);
+          }
+          _this.$element.show().scrollTop(0);
+          if (transition) {
+            _this.$element[0].offsetWidth;
+          }
+          _this.$element.addClass('in').attr('aria-hidden', false);
+          _this.enforceFocus();
+          e = $.Event('shown.modal', {
+            relatedTarget: _relatedTarget
+          });
+          if (transition) {
+            return _this.$element.find('.modal-dialog').one('miceTransitionEnd', function() {
+              return this.$element.trigger('focus').trigger(e);
+            }).emulateTransitionEnd(300);
+          } else {
+            return _this.$element.trigger('focus').trigger(e);
+          }
+        };
+      })(this));
+    };
+    Modal.prototype.hide = function(e) {
+      if (e) {
+        e.preventDefault();
+      }
+      e = $.Event('hide.modal');
+      this.$element.trigger(e);
+      if (!this.isShown || e.isDefaultPrevented()) {
+        return;
+      }
+      this.isShown = false;
+      this.$body.removeClass('modal-open');
+      this.resetScrollbar();
+      this.escape();
+      $(document).off('focusin.modal');
+      this.$element.removeClass('in').attr('aria-hidden', true).off('click.dismiss.modal');
+      if ($.support.transition && this.$element.hasClass('fade')) {
+        return this.$element.one('miceTransitionEnd', $.proxy(this.hideModal, this)).emulateTransitionEnd(300);
+      } else {
+        return this.hideModal();
+      }
+    };
+    Modal.prototype.enforceFocus = function() {
+      return $(document).off('focusin.modal').on('focusin.modal', $.proxy(function(e) {
+        if (this.$element[0] !== e.target && !this.$element.has(e.target).length) {
+          return this.$element.trigger('focus');
+        }
+      }, this));
+    };
+    Modal.prototype.escape = function() {
+      if (this.isShown && this.options.keyboard) {
+        return this.$element.on('keyup.dismiss.modal', $.proxy(function(e) {
+          return e.which === 27 && this.hide();
+        }), this);
+      } else if (!this.isShown) {
+        return this.$element.off('keyup.dismiss.modal');
+      }
+    };
+    Modal.prototype.hideModal = function() {
+      this.$element.hide();
+      return this.backdrop((function(_this) {
+        return function() {
+          return _this.$element.trigger('hidden.modal');
+        };
+      })(this));
+    };
+    Modal.prototype.removeBackdrop = function() {
+      this.$backdrop && this.$backdrop.remove();
+      return this.$backdrop = null;
+    };
+    Modal.prototype.backdrop = function(callback) {
+      var animate, callbackRemove, doAnimate;
+      animate = this.$element.hasClass('fade') ? 'fade' : '';
+      if (this.isShown && this.options.backdrop) {
+        doAnimate = $.support.transition && animate;
+        this.$backdrop = $('<div class="modal-backdrop ' + animate + '" />').appendTo(this.$body);
+        this.$element.on('click.dismiss.modal', $.proxy(function(e) {
+          if (e.target !== e.currentTarget) {
+            return;
+          }
+          if (this.options.backdrop === 'static') {
+            return this.$element[0].focus.call(this.$element[0]);
+          } else {
+            return this.hide.call(this);
+          }
+        }, this));
+        if (doAnimate) {
+          this.$backdrop[0].offsetWidth;
+        }
+        this.$backdrop.addClass('in');
+        if (!callback) {
+          return;
+        }
+        if (doAnimate) {
+          return this.$backdrop.one('miceTransitionEnd', callback).emulateTransitionEnd(150);
+        } else {
+          return callback();
+        }
+      } else if (!this.isShown && this.$backdrop) {
+        this.$backdrop.removeClass('in');
+        callbackRemove = (function(_this) {
+          return function() {
+            _this.removeBackdrop();
+            return callback && callback();
+          };
+        })(this);
+        if ($.support.transition && this.$element.hasClass('fade')) {
+          return this.$backdrop.one('miceTransitionEnd', callbackRemove).emulateTransitionEnd(150);
+        } else {
+          return callbackRemove();
+        }
+      } else if (callback) {
+        return callback();
+      }
+    };
+    Modal.prototype.checkScrollbar = function() {
+      if (document.body.clientWidth >= window.innerWidth) {
+        return;
+      }
+      return this.scrollbarWidth = this.scrollbarWidth || this.measureScrollbar();
+    };
+    Modal.prototype.setScrollbar = function() {
+      var bodyPad;
+      bodyPad = parseInt(this.$body.css('padding-right') || 0, 10);
+      if (this.scrollbarWidth) {
+        return this.$body.css('padding-right', bodyPad + this.scrollbarWidth);
+      }
+    };
+    Modal.prototype.resetScrollbar = function() {
+      return this.$body.css('padding-right', '');
+    };
+    Modal.prototype.measureScrollbar = function() {
+      var scrollDiv, scrollbarWidth;
+      scrollDiv = document.createElement('div');
+      scrollDiv.className = 'modal-scrollbar-measure';
+      this.$body.append(scrollDiv);
+      scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+      this.$body[0].removeChild(scrollDiv);
+      return scrollbarWidth;
+    };
+    Plugin = function(option, _relatedTarget) {
+      return this.each(function() {
+        var $element, data, options;
+        $element = $(this);
+        data = $element.data('modal');
+        options = $.extend({}, Modal.DEFAULTS, $element.data(), typeof option === 'object' && option);
+        if (!data) {
+          $element.data('modal', (data = new Modal(this, options)));
+        }
+        if (typeof option === 'string') {
+          return data[option](_relatedTarget);
+        } else if (options.show) {
+          return data.show(_relatedTarget);
+        }
+      });
+    };
+    old = $.fn.modal;
+    $.fn.modal = Plugin;
+    $.fn.modal.Constructor = Modal;
+    $.fn.modal.noConflict = function() {
+      $.fn.modal = old;
+      return this;
+    };
+    $(document).on('click.modal.data-api', '[data-toggle="modal"]', function(e) {
+      var $element, $target, href, option;
+      $element = $(this);
+      href = $element.attr('href');
+      $target = $($element.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, '')));
+      option = $target.data('modal') ? 'toggle' : $.extend({
+        remote: !/#/.test(href) && href
+      }, $target.data(), $element.data());
+      if ($element.is('a')) {
+        e.preventDefault();
+      }
+      $target.one('show.modal', function(showEvent) {
+        if (showEvent.isDefaultPrevented()) {
+          return;
+        }
+        return $target.one('hidden.modal', function() {
+          return $element.is(':visible') && $element.trigger('focus');
+        });
+      });
+      return Plugin.call($target, option, this);
+    });
+  })(jQuery);
 
-  Modal.prototype.setScrollbar = function () {
-    var bodyPad = parseInt((this.$body.css('padding-right') || 0), 10)
-    if (this.scrollbarWidth) this.$body.css('padding-right', bodyPad + this.scrollbarWidth)
-  }
-
-  Modal.prototype.resetScrollbar = function () {
-    this.$body.css('padding-right', '')
-  }
-
-  Modal.prototype.measureScrollbar = function () { // thx walsh
-    var scrollDiv = document.createElement('div')
-    scrollDiv.className = 'modal-scrollbar-measure'
-    this.$body.append(scrollDiv)
-    var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth
-    this.$body[0].removeChild(scrollDiv)
-    return scrollbarWidth
-  }
-
-
-  // MODAL PLUGIN DEFINITION
-  // =======================
-
-  function Plugin(option, _relatedTarget) {
-    return this.each(function () {
-      var $this   = $(this)
-      var data    = $this.data('bs.modal')
-      var options = $.extend({}, Modal.DEFAULTS, $this.data(), typeof option == 'object' && option)
-
-      if (!data) $this.data('bs.modal', (data = new Modal(this, options)))
-      if (typeof option == 'string') data[option](_relatedTarget)
-      else if (options.show) data.show(_relatedTarget)
-    })
-  }
-
-  var old = $.fn.modal
-
-  $.fn.modal             = Plugin
-  $.fn.modal.Constructor = Modal
-
-
-  // MODAL NO CONFLICT
-  // =================
-
-  $.fn.modal.noConflict = function () {
-    $.fn.modal = old
-    return this
-  }
-
-
-  // MODAL DATA-API
-  // ==============
-
-  $(document).on('click.bs.modal.data-api', '[data-toggle="modal"]', function (e) {
-    var $this   = $(this)
-    var href    = $this.attr('href')
-    var $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))) // strip for ie7
-    var option  = $target.data('bs.modal') ? 'toggle' : $.extend({ remote: !/#/.test(href) && href }, $target.data(), $this.data())
-
-    if ($this.is('a')) e.preventDefault()
-
-    $target.one('show.bs.modal', function (showEvent) {
-      if (showEvent.isDefaultPrevented()) return // only register focus restorer if modal will actually get shown
-      $target.one('hidden.bs.modal', function () {
-        $this.is(':visible') && $this.trigger('focus')
-      })
-    })
-    Plugin.call($target, option, this)
-  })
-
-}(jQuery);
+}).call(this);
 (function() {
   'use strict';
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
@@ -10276,7 +10237,7 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
       };
 
       Tooltip.prototype.show = function() {
-        var $parent, $tip, actualHeight, actualWidth, autoPlace, autoToken, calculatedOffset, complete, e, inDom, orgPlacement, parentDim, placement, pos, that;
+        var $parent, $tip, actualHeight, actualWidth, autoPlace, autoToken, calculatedOffset, complete, e, inDom, orgPlacement, parentDim, placement, pos;
         e = $.Event('show.mice.' + this.type);
         if (this.getTitle() && this.enabled) {
           this.$element.trigger(e);
@@ -10284,7 +10245,6 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
           if (e.isDefaultPrevented() || !inDom) {
             return;
           }
-          that = this;
           $tip = this.tip();
           this.setContent();
           if (this.options.animation) {
@@ -10318,10 +10278,12 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
           }
           calculatedOffset = this.getCalculatedOffset(placement, pos, actualWidth, actualHeight);
           this.applyPlacement(calculatedOffset, placement);
-          complete = function() {
-            that.$element.trigger('shown.mice.' + that.type);
-            return that.hoverState = null;
-          };
+          complete = (function(_this) {
+            return function() {
+              _this.$element.trigger('shown.mice.' + _this.type);
+              return _this.hoverState = null;
+            };
+          })(this);
           if ($.support.transition && this.$tip.hasClass('fade')) {
             return $tip.one('miceTransitionEnd', complete).emulateTransitionEnd(150);
           } else {
@@ -10375,16 +10337,17 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
       };
 
       Tooltip.prototype.hide = function() {
-        var $tip, complete, e, that;
-        that = this;
+        var $tip, complete, e;
         $tip = this.tip();
         e = $.Event('hide.mice.' + this.type);
-        complete = function() {
-          if (that.hoverState !== 'in') {
-            $tip.detach();
-          }
-          return that.$element.trigger('hidden.mice.' + that.type);
-        };
+        complete = (function(_this) {
+          return function() {
+            if (_this.hoverState !== 'in') {
+              $tip.detach();
+            }
+            return _this.$element.trigger('hidden.mice.' + _this.type);
+          };
+        })(this);
         this.$element.trigger(e);
         if (e.isDefaultPrevented()) {
           return;
