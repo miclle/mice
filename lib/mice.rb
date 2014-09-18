ad524ef64e54e0dcabccf44ff991d30cc2f7a342
Miclle <miclle.zheng@gmail.com>
2014-09-18 18:03:48 +0800

    Update mice, fix big bug

--- lib/mice.rb
+++ lib/mice.rb
@@ -1,18 +1,92 @@
 require "mice/version"
 
+# module Mice
+#   class << self
+#     # Inspired by Kaminari
+#     def load!
+
+#       if defined?(::Rails)
+#         require 'mice/engine'
+#       end
+
+#     end
+
+#   end
+
+# end
+
+# Mice.load!
+
 module Mice
   class << self
     # Inspired by Kaminari
     def load!
+      register_compass_extension if compass?
 
-      if defined?(::Rails)
-        require 'mice/engine'
+      if rails?
+        register_rails_engine
       end
 
+      configure_sass
     end
 
-  end
+    # Paths
+    def gem_path
+      @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
+    end
 
+    def stylesheets_path
+      File.join assets_path, 'stylesheets'
+    end
+
+    def fonts_path
+      File.join assets_path, 'fonts'
+    end
+
+    def javascripts_path
+      File.join assets_path, 'javascripts'
+    end
+
+    def assets_path
+      @assets_path ||= File.join gem_path, 'assets'
+    end
+
+    # Environment detection helpers
+    def asset_pipeline?
+      defined?(::Sprockets)
+    end
+
+    def compass?
+      defined?(::Compass)
+    end
+
+    def rails?
+      defined?(::Rails)
+    end
+
+    private
+
+    def configure_sass
+      require 'sass'
+
+      ::Sass.load_paths << stylesheets_path
+
+      # https://github.com/twbs/bootstrap-sass/issues/409
+      ::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
+    end
+
+    def register_compass_extension
+      ::Compass::Frameworks.register(
+          'mice',
+          :path                  => gem_path,
+          :stylesheets_directory => stylesheets_path
+      )
+    end
+
+    def register_rails_engine
+      require 'mice/engine'
+    end
+  end
 end
 
 Mice.load!
--- lib/mice/engine.rb
+++ lib/mice/engine.rb
@@ -2,7 +2,10 @@ module Mice
   module Rails
     class Engine < ::Rails::Engine
       initializer "mice.assets.precompile" do |app|
-        app.config.assets.precompile << %r(mice/fontawesome-webfont.eot\.(?:eot|svg|ttf|woff)$)
+        %w(stylesheets javascripts fonts images).each do |sub|
+          app.config.assets.paths << root.join('assets', sub)
+        end
+        app.config.assets.precompile << %r(mice/fontawesome-webfont\.(?:eot|svg|ttf|woff)$)
       end
     end
   end
